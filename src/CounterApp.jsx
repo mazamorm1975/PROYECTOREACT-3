8eba6879555b787dbb35befd77b5b635132eaa69
mazamora1975 <mzamora114@hotmail.com>
2023-12-15 11:56:49 -0600

    Cambios del modulo 47. Hook useState

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
 
-export const CounterApp=() =>{
-  return 
-  // eslint-disable-next-line no-unreachable
-  <>
-  
-  </>
-}
+// eslint-disable-next-line react/prop-types
+export const CounterApp = ({valor}) =>{
+
+  const [contar, setContar] = useState(valor);
+
+  const incrementarValor = () => setContar(c => c +1);
+
+  return (
+    <>
+    <h2>Contador Incrementador {contar}</h2>
+    <button
+      onClick={incrementarValor}
+    >Incrementar en 1</button>
+
+    </>
+  )}
